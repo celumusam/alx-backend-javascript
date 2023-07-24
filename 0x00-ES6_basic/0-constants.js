47348571cef31c158d0e25865f1175dd5539157a
Jerome Udoh <jeromeudoh7@gmail.com>
2023-07-24 13:14:36 +0000

    0. Const or let?

--- 0x00-ES6_basic/0-constants.js
+++ 0x00-ES6_basic/0-constants.js
@@ -1,23 +1,15 @@
-// Function taskFirst
 export function taskFirst() {
-  // Define a constant variable 'task'
   const task = 'I prefer const when I can.';
-  // Return the value of the 'task' constant
   return task;
 }
 
-// Function getLast
 export function getLast() {
-  // Return the string ' is okay'
   return ' is okay';
 }
 
-// Function taskNext
 export function taskNext() {
-  // Define a variable 'combination' and initialize it with the string 'But sometimes let'
   let combination = 'But sometimes let';
-  // Append the result of calling the 'getLast' function to the 'combination' variable
   combination += getLast();
-  // Return the concatenated value of 'combination'
+
   return combination;
 }
